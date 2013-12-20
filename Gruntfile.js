caa9390ff5f7ab4c209d107acb6f180b6a660257
Alexey Protasov <maleventum@gmail.com>
2013-12-20 17:06:37 +0400

    First commit (WIP)

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Flammenmensch
+ * Date: 20.12.13
+ * Time: 16:53
+ * To change this template use File | Settings | File Templates.
+ */
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+    });
+
+    grunt.registerTask('build', [ ]);
+};
---
+++ scripts/swf-directive.js
@@ -0,0 +1,73 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Flammenmensch
+ * Date: 20.12.13
+ * Time: 12:44
+ * To change this template use File | Settings | File Templates.
+ */
+(function (angular, swfobject) {
+    angular.module('flm.directives', [ ])
+        .directive('swfObject', function () {
+            return {
+                restrict: 'AE',
+                template: '<div></div>',
+                scope: {
+                    source:         '@',
+                    width:          '@',
+                    height:         '@',
+                    version:        '@',
+                    expressInstall: '@',
+                    params:         '=',
+                    flashvars:      '=',
+                    attributes:     '=',
+                    callback:       '&'
+                },
+                link: function (scope, iElement, iAttrs) {
+                    var params = {
+                        menu: false,
+                        quality: 'high',
+                        wmode: 'window',
+                        bgcolor: '#ffffff',
+                        allowscriptaccess: 'never',
+                        allowfullscreen: false
+                    };
+
+                    var flashvars = { };
+
+                    var attributes = {
+                        align: 'left',
+                        id: iElement[0].id
+                    };
+
+                    var fireEvent = function (type, data) {
+                        if (scope.callback && (typeof scope.callback === 'function')) {
+                            scope.callback({ type: type, data: data });
+                        }
+                    };
+
+                    var embedSwf = function () {
+                        swfobject.embedSWF(
+                            scope.source,
+                            iElement[0].id,
+                            scope.width || '100%',
+                            scope.height || '100%',
+                            scope.version || '11.9.0',
+                            scope.expressInstall || null,
+                            angular.extend(flashvars, scope.flashvars),
+                            angular.extend(params, scope.params),
+                            angular.extend(attributes, scope.attributes),
+                            function (event) {
+                                if (scope.callback && (typeof scope.callback === 'function')) {
+                                    scope.callback({ event: event });
+                                }
+                            }
+                        );
+                    };
+
+                    scope.$watch('source', function (newValue, oldValue, scope) {
+                        embedSwf();
+                    });
+                }
+            }
+        });
+} (window.angular, window.swfobject));
